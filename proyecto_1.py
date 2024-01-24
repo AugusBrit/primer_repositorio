b3e9e15bcb2f5d2a4b83a24bf703b71fe2cd5ce1
Augusto <augustogbt@gmail.com>
2024-01-24 00:45:31 -0300

    Ahora el archivo proyecto_1.py tiene un cálculo basado en la variable x

--- proyecto_1.py
+++ proyecto_1.py
@@ -1,10 +1,10 @@
 #hola este es mi primer proyecto
 
-def hola():
+def hola(x):
     print(f"Estoy contento porque estoy empezando en el mundo de Git.")
-
-hola()    
-
+    calculo_1 = x*3
+    print(calculo_1)
+hola(4)    
 
 
 
